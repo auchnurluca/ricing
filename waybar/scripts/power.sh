ed35df1f658f516b9ec92774eb0f6ad5cde41de3
eigentlichnurluca <s3005681@ba-sachsen.de>
2025-10-14 10:38:03 +0200

    :)

--- waybar/scripts/power.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CHOICE=$(echo -e "Shutdown\nReboot\nHyprlock\nSuspend" | wofi --dmenu)
-case $CHOICE in
-  Shutdown) systemctl poweroff ;;
-  Reboot) systemctl reboot ;;
-  Hyprlock) hyprctl dispatch dpms off ;;
-  Suspend) systemctl suspend ;;
-esac
--- waybar/scripts/song.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-song=$(playerctl -p spotify metadata --format "{{ artist }} - {{ title }}" 2>/dev/null)
-
-if [ -n "$song" ]; then
-    echo "$song"
-else
-    echo "  keine Musik"
-fi
